a955fc4530223cfe06371cae9672c61f778e925c
Ayman Ougri <103166756+waougri@users.noreply.github.com>
2026-05-20 23:37:18 -0400

    Create 1203-PrintInOrder

---
+++ 1203-PrintInOrder/1203-PrintInOrder.cpp
@@ -0,0 +1,44 @@
+// Last updated: 5/20/2026, 11:36:24 PM
+#include <condition_variable>
+#include <functional>
+#include <mutex>
+using std::function;
+class Foo {
+public:
+  std::mutex m;
+  std::condition_variable cv;
+  int turn;
+
+  Foo() {
+    turn = 0;
+  }
+
+  void first(function<void()> printFirst) {
+
+    // printFirst() outputs "first". Do not change or remove this line
+    printFirst();
+    turn = 1;
+    cv.notify_all();
+  }
+
+  void second(function<void()> printSecond) {
+    std::unique_lock<std::mutex> lock(m);
+    while(turn != 1) {
+        cv.wait(lock);
+    }
+    // printSecond() outputs "second". Do not change or remove this line.
+    printSecond();
+    turn = 2;
+    cv.notify_all();
+  }
+
+  void third(function<void()> printThird) {
+    std::unique_lock<std::mutex> lock(m);
+    while(turn != 2) {
+        cv.wait(lock);
+    }
+    // printThird() outputs "third". Do not change or remove this line.
+    printThird();
+    
+  }
+};
