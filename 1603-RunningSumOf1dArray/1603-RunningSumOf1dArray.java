568267173d11bcbe0d63d8d50bf3d417c68d72a6
Ayman Ougri <103166756+waougri@users.noreply.github.com>
2026-05-20 23:37:08 -0400

    Create 1603-RunningSumOf1dArray

---
+++ 1603-RunningSumOf1dArray/1603-RunningSumOf1dArray.java
@@ -0,0 +1,14 @@
+// Last updated: 5/20/2026, 11:36:14 PM
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Stack;
+class Solution {
+    public static int[] runningSum(int[] nums){
+        int[] runningSum = new int[nums.length];
+        runningSum[0]= nums[0];
+        for(int i = 1; i < nums.length; i++){
+            runningSum[i] = runningSum[i-1] + nums[i];
+        }
+        return runningSum;
+        }
+}
