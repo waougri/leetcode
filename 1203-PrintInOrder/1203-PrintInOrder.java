fd8f4f3013cd240c1ae5f44d64e706ee59f08427
Ayman Ougri <103166756+waougri@users.noreply.github.com>
2026-05-20 23:37:20 -0400

    Create 1203-PrintInOrder

---
+++ 1203-PrintInOrder/1203-PrintInOrder.java
@@ -0,0 +1,32 @@
+// Last updated: 5/20/2026, 11:36:26 PM
+class Foo {
+    private boolean oneDone;
+    private boolean twoDone;
+
+    public Foo() {
+        oneDone = false;
+        twoDone = false;
+    }
+
+    public synchronized void first(Runnable printFirst) throws InterruptedException {
+        printFirst.run();
+        oneDone = true;
+        notifyAll();
+    }
+
+    public synchronized void second(Runnable printSecond) throws InterruptedException {
+        while (!oneDone) {
+            wait();
+        }
+        printSecond.run();
+        twoDone = true;
+        notifyAll();
+    }
+
+    public synchronized void third(Runnable printThird) throws InterruptedException {
+        while (!twoDone) {
+            wait();
+        }
+        printThird.run();
+    }
+}
