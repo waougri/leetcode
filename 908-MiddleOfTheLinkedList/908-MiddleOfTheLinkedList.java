5ef2a95b61066469c0faeacaa70fb1921a204dac
Ayman Ougri <103166756+waougri@users.noreply.github.com>
2026-05-20 23:37:24 -0400

    Create 908-MiddleOfTheLinkedList

---
+++ 908-MiddleOfTheLinkedList/908-MiddleOfTheLinkedList.java
@@ -0,0 +1,12 @@
+// Last updated: 5/20/2026, 11:36:30 PM
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast!=null && fast.next!=null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+}
