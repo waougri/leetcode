c3c9520ffc1d80c5aa1ebe9f7451a0661d264e2d
Ayman Ougri <103166756+waougri@users.noreply.github.com>
2026-05-21 00:07:17 -0400

    Create 205-IsomorphicStrings

---
+++ 205-IsomorphicStrings/205-IsomorphicStrings.java
@@ -0,0 +1,53 @@
+// Last updated: 5/21/2026, 12:06:23 AM
+/*
+Given two strings s and t, determine if they are isomorphic.
+
+        Two strings s and t are isomorphic if the characters in s can be replaced to get t.
+
+        All occurrences of a character must be replaced with another character while preserving the order of characters. No two characters may map to the same character, but a character may map to itself.
+
+
+
+        Example 1:
+
+        Input: s = "egg", t = "add"
+        Output: true
+        Example 2:
+
+        Input: s = "foo", t = "bar"
+        Output: false
+        Example 3:
+
+        Input: s = "paper", t = "title"
+        Output: true
+
+
+        Constraints:
+
+        1 <= s.length <= 5 * 104
+        t.length == s.length
+        s and t consist of any valid ascii character.*/
+
+
+
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+        int[] sMap = new int[256];
+        int[] tMap = new int[256];
+        for (int i = 0; i < s.length(); i++) {
+            if (sMap[s.charAt(i)] != tMap[t.charAt(i)]) {
+                return false;
+            }
+            sMap[s.charAt(i)] = i + 1;
+            tMap[t.charAt(i)] = i + 1;
+        }
+        return true;
+        
+        
+
+
+    }
+}
