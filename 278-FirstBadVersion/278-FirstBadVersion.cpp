abfa6b34d88c9d662d25f2e98880daa80363292d
Ayman Ougri <103166756+waougri@users.noreply.github.com>
2026-05-20 23:37:42 -0400

    Create 278-FirstBadVersion

---
+++ 278-FirstBadVersion/278-FirstBadVersion.cpp
@@ -0,0 +1,36 @@
+// Last updated: 5/20/2026, 11:36:48 PM
+// The API isBadVersion is defined for you.
+// bool isBadVersion(int version);
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+class Solution {
+public:
+   int firstBadVersion(int n) {
+//        for (int i = 0; i <= n; i++) {
+//            if (isBadVersion(i))
+//            {
+//                return i;
+//            }else {
+//                continue;
+//            }
+//        }
+
+        int left = 1;
+        int right = n;
+        
+        while (left < right) {
+            int mid = floor(left + (right - left) / 2);
+            if (isBadVersion(mid))
+            {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+            
+        }
+
+        return left;
+    }
+};
